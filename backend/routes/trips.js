27b62537b6e170f5a18d98abd99bff15b368cc92
Harry <harpreetgoraya95@gmail.com>
2020-02-26 16:07:40 -0500

    added '/add' router

--- backend/routes/trips.js
+++ backend/routes/trips.js
@@ -7,3 +7,23 @@ router.route('/').get((req, res) => {
         .catch(err => res.status(400), json('Error while getting trips: ' + err));
 });
 
+router.route('/add').post((req, res) => {
+    const name = req.body.name;
+    const distance = req.body.distance;
+    const fuel_price = req.body.fuel_price;
+    const date = Date.parse(req.body.date);
+
+    const newTrip = new Trip({
+        name,
+        distance,
+        fuel_price,
+        date,
+    });
+
+    newTrip.save()
+        .then(() => res.json('Trip added'))
+        .catch(err => res.status(400).json('Error while adding trip: ' + err));
+
+});
+
+module.exports = router;
