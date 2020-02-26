4e6c0f4616b324127a60300375fc1f8109001d49
Harry <harpreetgoraya95@gmail.com>
2020-02-26 16:24:17 -0500

    Fixed '/' route error

--- backend/routes/trips.js
+++ backend/routes/trips.js
@@ -1,10 +1,11 @@
 const router = require('express').Router();
+
 let Trip = require('../models/trips.model');
 
 router.route('/').get((req, res) => {
     Trip.find()
-        .then(trips => res.json(trips))
-        .catch(err => res.status(400), json('Error while getting trips: ' + err));
+        .then(trip => res.json(trip))
+        .catch(err => res.status(400).json('Error while getting trips: ' + err));
 });
 
 router.route('/add').post((req, res) => {
