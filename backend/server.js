c69b95d33b500d7b929c6cd87f0d855420f35f23
Harry <harpreetgoraya95@gmail.com>
2020-02-26 15:56:56 -0500

    updated mongodb env file

--- backend/server.js
+++ backend/server.js
@@ -11,13 +11,14 @@ app.use(cors());
 app.use(express.json());
 
 const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
 
 const connection = mongoose.connection;
 connection.once('open', () => {
     console.log("MongoDB database connected");
-})
+});
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
-})
+});
+
