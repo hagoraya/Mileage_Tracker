8f8abd084dab4c99652a35b80aaa1304e937dfdd
Harry <harpreetgoraya95@gmail.com>
2020-02-26 16:55:03 -0500

    Create component files and imported them to app.js

--- mileage-app/src/App.js
+++ mileage-app/src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import "bootstrap/dist/css/bootstrap.min.css";
+
+
+import Navbar from "./components/navbar.component";
+import TripList from "./components/trip-list.component";
+import CreateTrip from "./components/create-trip.component";
+import EditTrip from "./components/edit-trip.component";
 
 function App() {
   return (
-    <div className="container">
-      Hello World
-    </div>
+    <Router>
+      <Navbar></Navbar>
+      <br></br>
+      <Route path="/" exact component={TripList}></Route>
+      <Route path="/create" component={CreateTrip}></Route>
+      <Route path="/edit/id" component={EditTrip}></Route>
+    </Router>
+
   );
 }
 
