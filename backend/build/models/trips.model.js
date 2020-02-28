6c3272c3ba8d3598ba506456738c9604908822c1
Harry <harpreetgoraya95@gmail.com>
2020-02-27 20:35:37 -0500

    created build script and built sever

---
+++ backend/build/models/trips.model.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const tripSchema = new Schema({
+    name: { type: String, required: true },
+    distance: { type: Number, required: true },
+    fuel_price: { type: Number, required: true },
+    date: { type: Date, default: Date.now }
+}, {
+    timestamps: true
+});
+
+const Trip = mongoose.model('Trip', tripSchema);
+
+module.exports = Trip;
