2cf6925b7144b592d3b063a53b824ac4777dc684
Harry <harpreetgoraya95@gmail.com>
2020-03-03 15:13:05 -0500

    fixed duplicate server file

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const cors = require('cors');
 const mongoose = require('mongoose');
 const express = require('express');
+const path = require('path');
 
 
 
@@ -16,6 +17,8 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+app.use(express.static(path.join(__dirname, "mileage-app", "build")))
+
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
 
@@ -28,6 +31,9 @@ const tripRouter = require('./routes/trips');
 
 app.use('/trips', tripRouter);
 
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "mileage-app", "build", "index.html"));
+})
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
