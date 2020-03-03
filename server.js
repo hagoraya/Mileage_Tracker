129208e1d8efec0a02acd1e0e431efaf4bb0e5f7
Harry <harpreetgoraya95@gmail.com>
2020-03-03 16:49:50 -0500

    changed temp port number

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const path = require('path');
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5999;
 
 
 //const staticFiles = express.static(path.join(__dirname, '../../client/build'))
