1268f4e7f7fc9f3c012a8833e053aae349307908
Harry <harpreetgoraya95@gmail.com>
2020-03-09 12:01:50 -0400

    added vehicle to trips model

--- models/trips.model.js
+++ models/trips.model.js
@@ -3,8 +3,9 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const tripSchema = new Schema({
-    name: { type: String, required: true },
+    name: { type: String },
     distance: { type: Number, required: true },
+    vehicle: { type: String, required: true },
     fuel_price: { type: Number, required: true },
     date: { type: Date, default: Date.now },
 }, {
